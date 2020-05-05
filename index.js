be378889747ae87d2b9e391acae92a71f4b77e3e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-05-04 23:39:53 -0400

    Still trying

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require('colors')
 const { readdir } = require('fs')
 const { join } = require('path')
 const { promisify } = require('util')
-const gpf = require('gpf-js')
+const { log, serve } = require('reserve')
+// const gpf = require('gpf-js')
 
 const readdirAsync = promisify(readdir)
 
@@ -17,11 +18,45 @@ async function main () {
 
   const baseUrl = `http://127.0.0.1:${port}`
 
-  console.log(await gpf.http.get(`${baseUrl}/`))
-  console.log(await gpf.http.get(`${baseUrl}/pages/about.html`))
+  // console.log(await gpf.http.get(`${baseUrl}/`))
+  // console.log(await gpf.http.get(`${baseUrl}/pages/about.html`))
 
-  const response = await gpf.http.get(`${baseUrl}/notes?bkid=3&_cmd=notes`)
-  console.log(response)
+  // const response = await gpf.http.request({
+  //   method: gpf.http.methods.get,
+  //   url: `${baseUrl}/backup`,
+  //   headers: {
+  //     'Accept': '*/*',
+  //     'Accept-Encoding': 'gzip, deflate',
+  //     'Accept-Language': 'en-US,en;q=0.8',
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.0 Safari/537.36',
+  //     // 'X-Requested-With': 'XMLHttpRequest',
+  //     // 'cookie': 'session-id=e3e0ec4a-7f31-4fef-ad69-d8ff2962aac9;',
+  //     'Referer': `${baseUrl}/pages/loading.html`,
+  //     'X-PRIVATE_APPKEY': '?!?',
+  //     // 'X-PRIVATE_APPKEY': '9c4d8ad6-8dbd-11ea-b95a-989096a0cb48',
+  //     'Connection': 'keep-alive',
+  //     'Host': `127.0.0.1:${port}`
+  //   }
+  // })
+  // console.log(response)
+
+  log(serve({
+    port: 8080,
+    mappings: [{
+      match: "^/$",
+      file: join(__dirname, 'index.html')
+    }, {
+      match: "^/(.*)",
+      url: `${baseUrl}/$1`,
+      'forward-request': ({ request: { method, url, headers }}) => {
+        console.log('before', headers)
+        headers.referer = `${baseUrl}/pages/loading.html`,
+        delete headers.cookie
+        Object.keys(headers).forEach(name => name.startsWith('sec-') ? delete headers[name] : 0)
+        console.log('after', headers)
+      }
+    }]
+  }), true)
 }
 
 main()
